5f3627ff5f9c34b0308d45c8b4b4a7ffc1d208f1
AOMuiz <abdulwasiumuiz@gmail.com>
2021-10-20 07:43:49 +0100

    added color pallete

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#fb2e86",
+  secondary: "#7E33E0",
+  offPurple: " #9F63B5",
+  blue: "#2F1AC4",
+  offBlue: "#151875",
+  red: "#FB2448",
+  offNavyBlue: "#3F509E",
+  pantonPurple: "#E0D3F5",
+};
