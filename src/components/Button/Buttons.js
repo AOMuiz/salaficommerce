4c28e4439b4daf75aab38806abc4bd7e8d17ea0d
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-10-26 14:12:04 +0100

    added a child element called: Button

--- src/components/Button/Buttons.js
+++ src/components/Button/Buttons.js
@@ -1,5 +1,9 @@
 const Button = () => {
-  return <div className="buttonContainer"></div>;
+  return (
+    <div className="buttonContainer">
+      <p>Button</p>
+    </div>
+  );
 };
 
 export default Button;
