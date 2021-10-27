ccf9f5e578f4f1551fec21f76e499b038538a04d
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-10-27 08:49:37 +0100

    comment out

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -28,10 +28,10 @@ const StyledContainer = styled.div`
   justify-content: space-between;
   margin: 1.5rem;
   transition: 0ms.25sec all ease-in-out;
-  cursor: pointer;
+  /* cursor: pointer;
   &:hover {
     box-shadow: 5px 5px 5px ${colors.skyBlue};
-  }
+  } */
 `;
 
 const ImageContainer = styled.div`
