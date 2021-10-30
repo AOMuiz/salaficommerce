c9f2063d6aaebe3a7169930e5f39f5069dc21602
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-10-30 22:25:07 +0100

    add some styles

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { colors } from "../../constants/colors";
 
-const Button = ({ name, backgroundColor }) => {
+const Button = ({ name, backgroundColor = colors.primary }) => {
   const handleClick = () => {
     console.log("button clicked");
   };
@@ -14,20 +15,21 @@ const Button = ({ name, backgroundColor }) => {
 };
 
 const StyledButton = styled.div`
-  width: 10.2rem;
+  width: ${(props) => (props.large ? "400px" : "500px")};
+  padding: 2rem 0.5rem;
   height: 3.13rem;
   background-color: ${(props) => props.backgroundColor};
+  border-radius: 2px;
   margin: 1rem 2.5rem 1rem 2.5rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 5px;
   cursor: pointer;
   &:hover {
-    background-color: ${(props) => props.backgroundColor};
-    color: blue;
+    background-color: ${colors.white};
+    color: ${(props) => props.backgroundColor};
     transition: 0.25s all ease-in-out;
-    box-shadow: 5px 5px 10px rgba(25, 47, 18, 0.3);
+    box-shadow: 3px 3px 7px rgba(25, 47, 18, 0.2);
   }
 `;
 
