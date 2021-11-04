1234375ef9cf03f9b53e4c6ad848cf4424545f2f
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-04 21:25:00 +0100

    comment out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import GlobalStyle from "./constants/styles/global";
-import HomeScreen from "./screens/HomeScreen";
+import cantileverChair from "./assets/images/cantileverChair.png";
+import comfortHandy from "./assets/images/comfortHandyChair.png";
+import Navbar from "./components/nav";
+import FashionItemCard from "./components/cards/FashionItemCard";
+import image from "./assets/images/rect.png";
+import image2 from "./assets/images/rect-2.png";
+// import ProductDetailsCard from "./components/cards/ProductDetailsCard";
+// import bag1 from "./assets/images/bag1.png";
+// import bag2 from "./assets/images/bag2.png";
+// import bag3 from "./assets/images/bag3.png";
 
 function App() {
   return (
---
+++ src/components/cards/ProductDetailsCard.js
@@ -0,0 +1,50 @@
+// import react from "react";
+// import styled from "styled-components";
+// import { colors } from "../../constants/colors";
+
+// const ProductDetailsCard = ({ url, url1, url2, url3, title }) => {
+//   return (
+//     <CardContainer>
+//       <SmallImageContainer>
+//         <Image1 src={url1} alt="" />
+//         <Image2 src={url2} alt="" />
+//         <Image3 src={url3} alt="" />
+//       </SmallImageContainer>
+//       <BigImageContainer>
+//         <BigImage src={url} alt="" />
+//       </BigImageContainer>
+//       <TextContainer>
+//         <Title>{title}</Title>
+//         <PriceContainer>
+//           <Price1></Price1>
+//           <Price2></Price2>
+//         </PriceContainer>
+//         <SubTitle></SubTitle>
+//       </TextContainer>
+//     </CardContainer>
+//   );
+// };
+
+// export default ProductDetailsCard;
+
+// const CardContainer = styled.div`
+//   width: 50px;
+//   height: 50px;
+//   background-color: ${colors.skyBlue};
+// `;
+
+// const SmallImageContainer = styled.div``;
+// const Image1 = styled.img``;
+// const Image2 = styled.img``;
+// const Image3 = styled.img``;
+
+// const BigImageContainer = styled.div``;
+
+// const BigImage = styled.img``;
+
+// const TextContainer = styled.div``;
+// const Title = styled.div``;
+// const PriceContainer = styled.div;
+// const Price1 = styled.div``;
+// const Price2 = styled.div``;
+// const SubTitle = styled.div``;
