9beb599f274efd2cb4b52199546329fb575655f8
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-05 21:05:52 +0100

    made some changes

--- src/components/cards/ProductDetailsCard.js
+++ src/components/cards/ProductDetailsCard.js
@@ -1,16 +1,18 @@
-const ProductDetailsCard = ({ url, url1 }) => {
+const ProductDetailsCard = ({ url, url1, url2, url3 }) => {
   return (
     <div className="grid grid-cols-3 gap-5">
       <div className="grid grid-rows-3 gap-5">
         <div>
-          <img src={url} alt="" />
-        </div>
-        <div>
           <img src={url1} alt="" />
         </div>
-        <div>1</div>
+        <div>
+          {" "}
+          <img src={url2} alt="" />
+          <img src={url3} alt="" />
+        </div>
       </div>
-      <div>2</div>
+      <img src={url} alt="" />
+      <div></div>
       <div>3</div>
     </div>
   );
