426114b701e0183fff49568c7d0f9554e29e360c
Kamaldeen Oluwaseun Jimoh <jimohkamal446@gmail.com>
2021-11-05 10:54:24 +0100

    Add rectangle component

---
+++ src/rectangle/index.js
@@ -0,0 +1,15 @@
+import facebook from "./assets/facebbok.svg";
+const Rectangle = () => {
+  return (
+    <div className="flex flex-row justify-around">
+      <div>©Webecy - All Rights Reserved {"&:copy"}</div>
+      <div>
+        <img src="./assets/facebook.svg" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Rectangle;
