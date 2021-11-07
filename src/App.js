66a545abc20ec1e9bcf030a795f3179f6d8fc857
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-07 22:57:34 +0100

    comment out HomeScreen component

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ import bag1 from "./assets/images/bag1.png";
 import bag2 from "./assets/images/bag2.png";
 import bag3 from "./assets/images/bag3.png";
 import HomeScreen from "./screens/HomeScreen";
+import ProductHeroSection from "./components/cards/ProductHeroSection";
 
 function App() {
   return (
     <>
-      <HomeScreen />s
+      <HomeScreen />
       <GlobalStyle />
       <div>
         <FashionItemCard
@@ -38,6 +39,14 @@ function App() {
       </div>
       <div>
         <ProductDetailsCard url={bag} url1={bag1} url1={bag2} url1={bag3} />
+      </div>
+      <div>
+        <ProductHeroSection
+          title="Product Details"
+          info1="Home . "
+          info2="Pages . "
+          info3="Product Details"
+        />
       </div>
     </>
   );
