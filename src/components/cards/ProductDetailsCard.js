7068534ba55fdbbfd9ce1bfde97f7c2605586194
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-07 22:10:11 +0100

    made some changes

--- src/components/cards/ProductDetailsCard.js
+++ src/components/cards/ProductDetailsCard.js
@@ -4,7 +4,13 @@ import styled from "styled-components";
 
 const ProductDetailsCard = ({ url, url1, url2, url3 }) => {
   return (
-    <CardContainer className="grid grid-cols-1 gap-5"></CardContainer>
+    <CardContainer className="grid grid-cols-3 gap-1">
+      <div className="grid grid-rows-3 gap-1">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </CardContainer>
 
     // <div className="grid grid-cols-3 gap-5">
     //   <div className="grid grid-rows-3 gap-5">
@@ -25,4 +31,6 @@ const ProductDetailsCard = ({ url, url1, url2, url3 }) => {
 
 export default ProductDetailsCard;
 
-const CardContainer = styled.div``;
+const CardContainer = styled.div`
+  background-color: ${colors.offPurple};
+`;
