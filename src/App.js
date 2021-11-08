0059bde423f72315e963ba4242f4fbbf66fd9412
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-08 20:24:49 +0100

    delete some imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import GlobalStyle from "./constants/styles/global";
-import cantileverChair from "./assets/images/cantileverChair.png";
-import comfortHandy from "./assets/images/comfortHandyChair.png";
-import Navbar from "./components/nav";
-import FashionItemCard from "./components/cards/FashionItemCard";
-import image from "./assets/images/rect.png";
-import image2 from "./assets/images/rect-2.png";
+
 import ProductDetailsCard from "./components/cards/ProductDetailsCard";
 import bag from "./assets/images/bag.png";
 import bag1 from "./assets/images/bag1.png";
@@ -16,27 +11,8 @@ import ProductHeroSection from "./components/cards/ProductHeroSection";
 function App() {
   return (
     <>
-      <HomeScreen />
-      <GlobalStyle />
-      <div>
-        <FashionItemCard
-          img={image}
-          title="My Chair"
-          price1="$200"
-          price2="$100"
-          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna
-           in est adipiscing in phasellus non in justo."
-        />
+      {/* <HomeScreen /> */}
 
-        <FashionItemCard
-          img={image2}
-          title="My Room"
-          price1="$2,000"
-          price2="$1,000"
-          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna
-           in est adipiscing in phasellus non in justo."
-        />
-      </div>
       <div>
         <ProductDetailsCard url={bag} url1={bag1} url1={bag2} url1={bag3} />
       </div>
@@ -48,6 +24,7 @@ function App() {
           info3="Product Details"
         />
       </div>
+      <GlobalStyle />
     </>
   );
 }
