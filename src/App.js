edce97a3560768d5436ed3ac3ae6a605983fd7b5
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-08 19:29:45 +0100

    umcomment HomeScreen and import HomeScreen

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import bag from "./assets/images/bag.png";
 import bag1 from "./assets/images/bag1.png";
 import bag2 from "./assets/images/bag2.png";
 import bag3 from "./assets/images/bag3.png";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
-      {/* <HomeScreen /> */}
+      <HomeScreen />s
       <GlobalStyle />
       <div>
         <FashionItemCard
