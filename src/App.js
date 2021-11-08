73f66dc3c05e7cb04b2168eee1dae1f3ab358b9a
AOMuiz <abdulwasiumuiz@gmail.com>
2021-11-08 20:57:10 +0100

    test product details component and remove from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import GlobalStyle from "./constants/styles/global";
-
-import ProductDetailsCard from "./components/cards/ProductDetailsCard";
-import bag from "./assets/images/bag.png";
-import bag1 from "./assets/images/bag1.png";
-import bag2 from "./assets/images/bag2.png";
-import bag3 from "./assets/images/bag3.png";
 import HomeScreen from "./screens/HomeScreen";
-import ProductHeroSection from "./components/cards/ProductHeroSection";
 
 function App() {
   return (
