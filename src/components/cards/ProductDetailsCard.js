a35457b2b831999932ed15408f1833c1e929ea2e
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-08 20:25:30 +0100

    delete some things

--- src/components/cards/ProductDetailsCard.js
+++ src/components/cards/ProductDetailsCard.js
@@ -3,24 +3,7 @@ import { colors } from "../../constants/colors";
 import styled from "styled-components";
 
 const ProductDetailsCard = ({ url, url1, url2, url3 }) => {
-  return (
-    <CardContainer></CardContainer>
-
-    // <div className="grid grid-cols-3 gap-5">
-    //   <div className="grid grid-rows-3 gap-5">
-    //     <div>
-    //       <img src={url1} alt="" />
-    //     </div>
-    //     <div>
-    //       <img src={url2} alt="" />
-    //       <img src={url3} alt="" />
-    //     </div>
-    //   </div>
-    //   <img src={url} alt="" />
-    //   <div></div>
-    //   <div>3</div>
-    // </div>
-  );
+  return <CardContainer></CardContainer>;
 };
 
 export default ProductDetailsCard;
