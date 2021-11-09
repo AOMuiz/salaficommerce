ec3281c700fd789d03c5923fcedb8800ff9d31c0
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-09 13:27:19 +0100

    add some props to the ProductDetailsCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import GlobalStyle from "./constants/styles/global";
 
 import ProductDetailsCard from "./components/cards/ProductDetailsCard";
-import bag from "./assets/images/bag.png";
+import bigbag from "./assets/images/big-bag.png";
 import bag1 from "./assets/images/bag1.png";
 import bag2 from "./assets/images/bag2.png";
 import bag3 from "./assets/images/bag3.png";
@@ -14,16 +14,24 @@ function App() {
       {/* <HomeScreen /> */}
 
       <div>
-        <ProductDetailsCard url={bag} url1={bag1} url1={bag2} url1={bag3} />
+        <ProductDetailsCard
+          url={bigbag}
+          url1={bag1}
+          url2={bag2}
+          url3={bag3}
+          title="Playwood arm chair"
+          price1="$100"
+          price2="$200"
+        />
       </div>
-      <div>
+      {/* <div>
         <ProductHeroSection
           title="Product Details"
           info1="Home . "
           info2="Pages . "
           info3="Product Details"
         />
-      </div>
+      </div> */}
       <GlobalStyle />
     </>
   );
