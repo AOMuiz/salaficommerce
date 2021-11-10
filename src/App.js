0399c1008c926e6b0e61b670bcf97620c42bcbf1
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-10 21:27:04 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import ProductCard from "./components/cards/ProductCard";
 import GlobalStyle from "./constants/styles/global";
 import HomeScreen from "./screens/HomeScreen";
+import img from "./assets/images/img1.png";
 
 function App() {
   return (
     <>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
       {/* <div>
         <ProductDetailsCard url={bag} url1={bag1} url2={bag2} url3={bag3} />
       </div> */}
       <GlobalStyle />
+      <div>
+        <ProductCard
+          url={img}
+          title="23% off in all products"
+          subtitle="Shop Now"
+        />
+      </div>
     </>
   );
 }
