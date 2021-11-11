0f48b4f962ba63f5ab65fc486b8bfc73481c4bac
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-11 19:10:06 +0100

    add props

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -2,14 +2,9 @@ import React from "react";
 import { colors } from "../../constants/colors";
 import styled from "styled-components";
 
-const ProductCard = ({
-  url,
-  title,
-  subtitle,
-  backgroundcolor = colors.primary,
-}) => {
+const ProductCard = ({ url, title, subtitle, color = colors.primary }) => {
   return (
-    <CardContainer>
+    <CardContainer className="p-2 ">
       <TextContainer>
         <Title>{title}</Title>
         <div>{subtitle}</div>
@@ -24,18 +19,17 @@ const ProductCard = ({
 export default ProductCard;
 
 const CardContainer = styled.div`
-  background-color: ${(props) => props.backgroundcolor};
+  background-color: ${(props) => props.color};
   margin: 10px;
   height: 14rem;
   width: 24rem;
   box-shadow: 0.5rem 0.5rem 0.5rem ${colors.lightArsh};
 `;
 
-const TextContainer = styled.div``;
-const ImageContainer = styled.div`
-  height: 5rem;
-  width: 5rem;
+const TextContainer = styled.div`
+  font-size: 1.2rem;
 `;
+const ImageContainer = styled.div``;
 
 const Image = styled.div``;
 const Title = styled.div`
