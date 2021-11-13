baff51c0db40c35251827ac4a6bb310bfbb97cdc
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-13 23:08:21 +0100

    creat a new component, imort an image and made some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import TopCategoriesCard from "./components/cards/TopCategoriesCard";
 import GlobalStyle from "./constants/styles/global";
 import HomeScreen from "./screens/HomeScreen";
+import img from "./assets/images/image-20.png";
+import { colors } from "./constants/colors";
 
 function App() {
   return (
     <>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
       {/* <div>
         <ProductDetailsCard url={bag} url1={bag1} url2={bag2} url3={bag3} />
       </div> */}
       <GlobalStyle />
+      <div>
+        <TopCategoriesCard url={img} name="show" />
+      </div>
     </>
   );
 }
---
+++ src/components/cards/TopCategoriesCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import { colors } from "../../constants/colors";
+import Button from "../button";
+
+const TopCategoriesCard = ({ url, name, height }) => {
+  return (
+    <CardContiner>
+      <div>
+        <Image src={url} alt="" />
+        <Button name={name} height={height} />
+      </div>
+    </CardContiner>
+  );
+};
+
+export default TopCategoriesCard;
+
+const CardContiner = styled.div`
+  width: 18rem;
+  height: 18rem;
+  background-color: #f6f7fb;
+  margin: 20px;
+  border-radius: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  box-shadow: 0.3rem 0.3rem 2rem ${colors.lightArsh};
+  &:hover {
+    transition: 0.25s all ease-in-out;
+    box-shadow: -0.5rem 1.2rem 0.7rem ${colors.lightBlack};
+  }
+`;
+
+const Image = styled.img`
+  width: 10rem;
+  height: 10rem;
+`;
