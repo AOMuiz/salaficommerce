cfa139f3515b65e959482749d1289f541d9a90ba
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-15 17:05:48 +0100

    made many changes to the TopCategoriesCard component

--- src/components/cards/TopCategoriesCard.js
+++ src/components/cards/TopCategoriesCard.js
@@ -4,18 +4,18 @@ import { colors } from "../../constants/colors";
 
 const TopCategoriesCard = ({ url, name, buttonName }) => {
   return (
-    <CardContiner>
-      <ButtonContainer>
+    <CardContainer className="parent">
+      <ImageContainer>
         <Image src={url} alt="" />
-      </ButtonContainer>
-      <Button>{buttonName}</Button>
-    </CardContiner>
+      </ImageContainer>
+      <Button className="button">{buttonName}</Button>
+    </CardContainer>
   );
 };
 
 export default TopCategoriesCard;
 
-const CardContiner = styled.div`
+const CardContainer = styled.div`
   width: 18rem;
   height: 18rem;
   background-color: #f6f7fb;
@@ -27,23 +27,29 @@ const CardContiner = styled.div`
   flex-direction: column;
   cursor: pointer;
   box-shadow: 0.3rem 0.3rem 2rem ${colors.lightArsh};
-  &:hover {
+  :hover {
     transition: 0.25s all ease-in-out;
-    box-shadow: -0.5rem 1.2rem 0.7rem ${colors.lightBlack};
+    box-shadow: -0.5rem 1.2rem 0.5rem #9877e7;
+
+    [class~="button"] {
+      display: block;
+      transition: 0.2s all ease-in;
+    }
   }
 `;
 
-const ButtonContainer = styled.div``;
+const ImageContainer = styled.div``;
 
 const Button = styled.div`
   padding: 0.3rem 0.6rem;
-  border-radius: 0.5rem;
-  background-color: rgba(10, 200, 100, 1);
-  /* height: 3rem;
-  width: 4rem; */
+  border-radius: 0.2rem;
+  background-color: #08d15f;
+  color: ${colors.white};
+  display: none;
 `;
 
 const Image = styled.img`
   width: 10rem;
   height: 10rem;
+  transition: 0.25s all ease-in;
 `;
