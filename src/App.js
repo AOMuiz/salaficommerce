b40c9b3e3528548f443720c794ad04e89963b32d
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-11-15 17:04:53 +0100

    import three images to be passed as props to the TopCategoriesCard component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import TopCategoriesCard from "./components/cards/TopCategoriesCard";
 import GlobalStyle from "./constants/styles/global";
 import HomeScreen from "./screens/HomeScreen";
 import img from "./assets/images/image-20.png";
+import img2 from "./assets/images/image-30.png";
+import img3 from "./assets/images/image-40.png";
+import img4 from "./assets/images/image-50.png";
 import { colors } from "./constants/colors";
 
 function App() {
@@ -12,8 +15,11 @@ function App() {
         <ProductDetailsCard url={bag} url1={bag1} url2={bag2} url3={bag3} />
       </div> */}
       <GlobalStyle />
-      <div>
-        <TopCategoriesCard url={img} buttonName="show" />
+      <div className="grid grid-cols-4">
+        <TopCategoriesCard url={img} buttonName="View Shop" />
+        <TopCategoriesCard url={img2} buttonName="View Shop" />
+        <TopCategoriesCard url={img3} buttonName="View Shop" />
+        <TopCategoriesCard url={img4} buttonName="View Shop" />
       </div>
     </>
   );
