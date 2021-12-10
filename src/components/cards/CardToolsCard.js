49e603e79f768ec5e535deb4f72b72785bef5a3f
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-12-10 22:40:02 +0100

    create a CartTotalsCard component

---
+++ src/components/cards/CardToolsCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const CartTotalsCard = () => {
+  return;
+  <div className="grid grid-rows-4">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+  </div>;
+};
+
+export default CartTotalsCard;
