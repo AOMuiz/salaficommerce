98787a53773b6e0d58a2e7a73c2b38d0aa055f7e
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-12-15 14:56:33 +0100

    passed some props

--- src/components/cards/CardToolsCard.js
+++ src/components/cards/CardToolsCard.js
@@ -1,13 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-const CartTotalsCard = () => {
+const CartTotalsCard = ({
+  subtotal,
+  price,
+  total,
+  price2,
+  text,
+  buttontext,
+}) => {
   return (
-    <Container className="grid-cols-4">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <Container className="grid-cols-4 flex just px-10">
+      <div className="flex justify-between">
+        <div>{subtotal}</div>
+        <div>{price}</div>
+      </div>
+      <div className="flex justify-between">
+        <div>{total}</div>
+        <div>{price2}</div>
+      </div>
+      <div>{text}</div>
+      <Button>{buttontext}</Button>
     </Container>
   );
 };
@@ -15,7 +28,18 @@ const CartTotalsCard = () => {
 export default CartTotalsCard;
 
 const Container = styled.div`
-  background-color: blue;
-  height: 10rem;
-  width: 100%;
+  background-color: #f4f4fc;
+  height: 30rem;
+  width: 40rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const Button = styled.div`
+  background-color: #19d16f;
+  border-radius: 0.2rem;
+  text-align: center;
+  padding: 1rem 0rem;
+  color: white;
 `;
