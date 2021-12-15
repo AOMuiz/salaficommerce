c7c356483c436281c654f7724589aa0ff281028a
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2021-12-15 14:09:58 +0100

    made some changes

--- src/components/cards/CardToolsCard.js
+++ src/components/cards/CardToolsCard.js
@@ -2,17 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 const CartTotalsCard = () => {
-  return;
-  <Container className="grid grid-rows-4">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>4</div>
-  </Container>;
+  return (
+    <Container className="grid-cols-4">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Container>
+  );
 };
 
 export default CartTotalsCard;
 
 const Container = styled.div`
-  background-color: #f4f4fc;
+  background-color: blue;
+  height: 10rem;
+  width: 100%;
 `;
