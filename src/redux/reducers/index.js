f99fe425b3ebc94d34a41377c981af526d2fa9b7
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2022-01-10 23:56:44 +0100

    import addCart and deleteCart to combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
+import addCart from "../../state/reducers/addCart";
+import deleteCart from "../../state/reducers/deleteCart";
 import { productReducer, selectedProductReducer } from "./productReducer";
 
 const rootReducer = combineReducers({
   allProducts: productReducer,
   product: selectedProductReducer,
+  add: addCart,
+  delete: deleteCart,
 });
 
 export default rootReducer;
